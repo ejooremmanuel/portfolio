30aa80d41f975d25eabb9446854fda5a4ac5d92b
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-10-13 23:29:28 +0100

    fixing changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { ThemeProvider } from "../src/Context";
+import { ThemeProvider } from "./Context";
 
 ReactDOM.render(
   <ThemeProvider>
