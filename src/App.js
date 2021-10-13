65507560a1e4d280a3dfe99031bc02c60efce35a
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-10-13 23:58:36 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import "semantic-ui-css/semantic.min.css";
 import Intro from "./components/intro/Intro";
 import Toggle from "./components/toggle/Toggle";
-import { ThemeContext } from "../src/Context";
+import { ThemeContext } from "./Context";
 function App() {
   const theme = useContext(ThemeContext);
   const { darkMode } = theme.state;
